c6ad15e1dd2187238a54abb9befaf17e2f6731e7
Nastya <anastasiiya.p@gmail.com>
2021-02-06 17:16:05 +0200

    first

---
+++ Lesson1/js/script.js
@@ -0,0 +1,24 @@
+"use strict";
+// const obj = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.name);
+
+// let arr = ['orange', 'apple', 1, {}];
+// console.log(arr[1]);
+
+// alert('vu,v.gvb');
+// let conf = confirm('Are you here?');
+// console.log(conf);
+
+// const ans = prompt("How old are you?");
+// console.log(ans + 5);
+
+const answers = [];
+answers[0] = prompt('What is your first name?');
+answers[1] = prompt('What is your last name?');
+answers[2] = prompt('How old are you?');
+document.write(answers);
