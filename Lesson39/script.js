19c4551ecb9106c273859bcc1884a8c8be341b22
Nastya <anastasiiya.p@gmail.com>
2021-03-12 07:48:05 +0200

    1203

--- Lesson39/script.js
+++ Lesson39/script.js
@@ -24,4 +24,17 @@ let timerId,
 function myAnimation() {
     const elem = document.querySelector('.box');
     let pos = 0;
+
+    const id = setInterval(frame, 10); 
+    function frame() {
+        if (pos == 300) {
+            clearInterval();
+        } else {
+            pos++;
+            elem.style.top = pos + "px";
+            elem.style.left = pos + "px";
+        }
+    };
 };
+
+btn.addEventListener('click', myAnimation);
