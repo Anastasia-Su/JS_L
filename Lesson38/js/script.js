72cdb5cdbb841552489da76f63b9353a9ceb5808
Nastya <anastasiiya.p@gmail.com>
2021-03-18 09:19:21 +0200

    0919

--- Lesson38/js/script.js
+++ Lesson38/js/script.js
@@ -233,8 +233,8 @@ function postData(form) {
 
         const request = new XMLHttpRequest();
         request.open('POST', 'server.php');
-        request.setRequestHeader('Content-type', 'multipart/form-data');
-        const formData = new formData(form);
+        // request.setRequestHeader('Content-type', 'multipart/form-data');
+        const formData = new FormData(form);
 
         request.send(formData);
         request.addEventListener('load', () => {
